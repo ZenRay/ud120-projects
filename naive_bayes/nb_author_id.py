0c14a72964356b85a7183932b3b78bc5d96daa9a
ZenRay <jake2011ren@gmail.com>
2017-11-26 22:17:42 +0800

    import sklearn package;test the email authors.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,11 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+# import sklearn naive_bayes
+from sklearn.naive_bayes import GaussianNB
+# import bayes score
+from sklearn.metrics import accuracy_score
+
 
 
 ### features_train and features_test are the features for the training
@@ -26,7 +31,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+clf = GaussianNB() # GaussianNB classifier
+clf.fit(features_train, labels_train)
+prediction = clf.predict(features_test) # get the predition label
+print accuracy_score(prediction, labels_test)
 
 #########################################################
 
