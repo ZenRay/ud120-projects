3a9a719ef939d039607ddcb6d2fccae430c46f57
ZenRay <jake2011ren@gmail.com>
2017-11-27 14:16:06 +0800

    choose the kernel linear or rbf

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,23 +21,44 @@ from sklearn.metrics import accuracy_score
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+# takes long time to run the svm
+# reduce the number of dataset to run more quickly
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
-clf = SVC(kernel="linear")
-# train the dataset and running time
-t0 = time()
-clf.fit(features_train, labels_train)
-print "Training time %f s\n" % round(time() - t0, 3)
-# predict the test dataset and running time
-t1 = time()
-prediction = clf.predict(features_test)
-print "Prediction time %f s\n" % round(time() - t1, 3)
-# get the prediction accuracy
-print("Prediciton accuracy score:\n")
-print accuracy_score(prediction, labels_test)
+kernel = raw_input("Enter the kernel function: ")
+
+# linear kernel
+if kernel == "linear":
+	clf = SVC(kernel="linear")
+	# train the dataset and running time
+	t0 = time()
+	clf.fit(features_train, labels_train)
+	print "Training time %f s\n" % round(time() - t0, 3)
+	# predict the test dataset and running time
+	t1 = time()
+	prediction = clf.predict(features_test)
+	print "Prediction time %f s\n" % round(time() - t1, 3)
+	# get the prediction accuracy
+	print("Prediciton accuracy score:\n")
+	print accuracy_score(prediction, labels_test)
+
+# rbf kernel
+elif kernel == "rbf":
+	clf = SVC(kernel="rbf")
+	# train the dataset and running time
+	t0 = time()
+	clf.fit(features_train, labels_train)
+	print "Training time %f s\n" % round(time() - t0, 3)
+	# predict the test dataset and running time
+	t1 = time()
+	prediction = clf.predict(features_test)
+	print "Prediction time %f s\n" % round(time() - t1, 3)
+	# get the prediction accuracy
+	print("Prediciton accuracy score:\n")
+	print accuracy_score(prediction, labels_test)
 
 
 #########################################################
