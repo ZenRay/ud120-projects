4e91a25963496959ccef26a11e4b50d7fc71c8e9
ZenRay <jake2011ren@gmail.com>
2017-11-27 13:43:45 +0800

    import the svc class, train dataset and predict dataset

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,8 +12,10 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
-
+# import svc
+from sklearn.svm import SVC
+# import accuracy score
+from sklearn.metrics import accuracy_score
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -24,6 +26,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = SVC(kernel="linear")
+# train the dataset
+clf.fit(features_train, labels_train)
+# predict the test dataset
+prediction = clf.predict(features_test)
+# get the prediction accuracy
+print accuracy_score(prediction, labels_test)
+
 
 #########################################################
 
