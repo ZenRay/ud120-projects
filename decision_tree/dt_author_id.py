4ea295b8f6428e1e8690eb044601c3283de3fe56
ZenRay <jake2011ren@gmail.com>
2017-11-27 19:48:59 +0800

    using the min_sample_split to create decision tree and get the prediction accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,9 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+# import decision tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,8 +26,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+prediction = clf.predict(features_test)
 
 
+accuracy = accuracy_score(prediction, labels_test)
+print "The prediction accuracy score is %f ." % accuracy
 #########################################################
 
 
