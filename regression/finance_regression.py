7686e6bc69bf1e81c27799798a56d365290a1eff
ZenRay <jake2011ren@gmail.com>
2017-11-28 21:21:11 +0800

    change the feature and test regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -24,7 +24,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "long_term_incentive"] #, "salary"]
 # avoid the dict keys sequence after py3.3
 sort_keys = "../tools/python2_lesson06_keys.pkl"
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
