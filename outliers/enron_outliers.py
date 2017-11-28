383267f20b30c59e99072bcb94144a53a58573c3
ZenRay <jake2011ren@gmail.com>
2017-11-29 02:16:16 +0800

    remove the outlier creating by software bug

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,11 +10,13 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+# remove the outlier
+data_dict.pop("TOTAL", 0)
 data = featureFormat(data_dict, features)
 
 
 ### your code below
-a = 0
 for point in data:
     salary = point[0]
     bonus = point[1]
