8a8e39a06746fb0e61c133451b1e900c9077eea0
ZenRay <jake2011ren@gmail.com>
2017-11-29 02:50:05 +0800

    remove the outlier creating by software bug

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,8 +10,7 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
-
-# remove the outlier
+#remove outlier
 data_dict.pop("TOTAL", 0)
 data = featureFormat(data_dict, features)
 
@@ -21,6 +20,7 @@ for point in data:
     salary = point[0]
     bonus = point[1]
     matplotlib.pyplot.scatter( salary, bonus )
+
 
 matplotlib.pyplot.xlabel("salary")
 matplotlib.pyplot.ylabel("bonus")
