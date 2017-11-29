1e1fb5c8558a6797bc466c9140c43ec939311923
ZenRay <jake2011ren@gmail.com>
2017-11-30 00:00:11 +0800

    use stemmer to parse the sentence

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,19 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        stemmer = SnowballStemmer("english")
+        tem_words = []
+        for word in text_string.split(" "):
+            # check the word whether it's space
+            if word != "":
+                tem_words.append(stemmer.stem(word))
 
-
-
+        words = " ".join(tem_words)
 
     return words
 
