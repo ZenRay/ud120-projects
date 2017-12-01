1b50131450e8fe639d50dbb3d12d388a7d7889c3
ZenRay <jake2011ren@gmail.com>
2017-12-02 01:36:41 +0800

    create decision tree&get the score

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,19 +14,26 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+# import cross_validation & decisiontree
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(
+    open("../final_project/final_project_dataset.pkl", "r"))
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+# it's all yours from here forward!
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.2, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
 
-### it's all yours from here forward!  
-
-
+print "The decision tree score is %f ." % clf.score(features, labels)
