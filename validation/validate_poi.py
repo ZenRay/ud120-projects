ea83f6f94d5a4259fa429e0d1ca8ce93f2e8339d
ZenRay <jake2011ren@gmail.com>
2017-12-02 01:40:08 +0800

    use cross validation to test the score

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -32,7 +32,7 @@ labels, features = targetFeatureSplit(data)
 
 # it's all yours from here forward!
 features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.2, random_state=42)
+    train_test_split(features, labels, test_size=0.3, random_state=42)
 clf = DecisionTreeClassifier()
 clf.fit(features_train, labels_train)
 
