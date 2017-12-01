01f28bbdbc35820fbd25a801ca23064f695a27e7
ZenRay <jake2011ren@gmail.com>
2017-12-01 21:04:13 +0800

    test the various n_component

--- pca/test_component.py
+++ pca/test_component.py
@@ -115,6 +115,37 @@ print classification_report(y_test, y_pred, target_names=target_names)
 print confusion_matrix(y_test, y_pred, labels=range(n_classes))
 
 
+# Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
+# dataset): unsupervised feature extraction / dimensionality reduction
+for n_components in [10, 25, 50, 100, 250]:
+    pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+    eigenfaces = pca.components_.reshape((n_components, h, w))
+    print "Projecting the input data on the eigenfaces orthonormal basis"
+    X_train_pca= pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
+
+
+###############################################################################
+# Train a SVM classification model
+
+    print "Fitting the classifier to the training set"
+    param_grid = {
+             'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+              }
+# for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
+    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+    clf = clf.fit(X_train_pca, y_train)
+
+
+
+###############################################################################
+# Quantitative evaluation of the model quality on the test set
+
+
+    y_pred = clf.predict(X_test_pca)
+    print 'n_components' + classification_report(y_test, y_pred, target_names=target_names). split("\n")[0]
+    print "{0:12d}".format(n_components)+classification_report(y_test, y_pred, target_names=target_names). split("\n")[2]
 ###############################################################################
 # Qualitative evaluation of the predictions using matplotlib
 
